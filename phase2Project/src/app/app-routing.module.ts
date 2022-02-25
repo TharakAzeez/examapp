b4f5378d3a331a32165c9b12d96e7c45136a9945
tharak <tharakazeez2001@gmail.com>
2022-02-25 11:53:59 +0530

    first commit

--- phase2Project/src/app/app-routing.module.ts
+++ phase2Project/src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExamComponent } from './m1/exam/exam.component';
+import { FirstComponent } from './m1/first/first.component';
 import { LoginComponent } from './m1/login/login.component';
 import { SignupComponent } from './m1/signup/signup.component';
 
 const routes: Routes = [
   { path:'login',component:LoginComponent},
-  {path:'signup',component:SignupComponent}
+  {path:'signup',component:SignupComponent},
+  {path:'',component:FirstComponent},
+  {path:'exam',component:ExamComponent}
 
 ];
 
--- phase2Project/src/app/app.module.ts
+++ phase2Project/src/app/app.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule ,routingcomponent} from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { M1Module } from './m1/m1.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingcomponent
+    
   ],
   imports: [
     BrowserModule,
